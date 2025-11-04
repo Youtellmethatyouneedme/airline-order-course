acedcd13411c871623c652ad6ce78683459cfc1f
Youtellmethatyouneedme <47274235+Youtellmethatyouneedme@users.noreply.github.com>
2025-11-04 11:06:03 +0800

    公网IP修改

--- backend/src/main/java/com/postion/airlineorderbackend/config/SwaggerConfig.java
+++ backend/src/main/java/com/postion/airlineorderbackend/config/SwaggerConfig.java
@@ -26,7 +26,7 @@ public class SwaggerConfig {
                         .url("https://github.com/tianyf925"))
                 )
                 .servers(java.util.Arrays.asList(
-                        new Server().url("http://13.60.37.17:8080"),
+                        new Server().url("http://16.171.169.189:8080"),
                         new Server().url("http://api.airline.com")
                 ))
                 .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
